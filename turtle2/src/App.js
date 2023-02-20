846f6c85dd48bf383ed683fc3ee3764544d6df63
John Doe <johndoe@example.com>
2023-02-20 20:34:53 +0800

    enter

--- turtle2/src/App.js
+++ turtle2/src/App.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import { Box,Toolbar,AppBar, Icon, Button } from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
+import Topbar from './components/Topbar';
+import LoginPart from './components/LoginPart';
 
 
 const darkTheme = createTheme({
@@ -15,28 +13,11 @@ const darkTheme = createTheme({
 
 function App() {
   return (
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
+    <ThemeProvider theme={darkTheme} >
+      <CssBaseline/>
       <div>
-      <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
+        <Topbar></Topbar>
+          <LoginPart></LoginPart>
       </div>
     </ThemeProvider>
   );
